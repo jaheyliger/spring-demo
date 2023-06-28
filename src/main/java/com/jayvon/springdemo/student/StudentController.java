12dc6586718347922374d2f4a1483afd06493174
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2023-06-28 12:51:27 -0400

    added CRUD routes

--- src/main/java/com/jayvon/springdemo/student/StudentController.java
+++ src/main/java/com/jayvon/springdemo/student/StudentController.java
@@ -1,8 +1,6 @@
 package com.jayvon.springdemo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +14,28 @@ public class StudentController {
     public StudentController(StudentService service) {
         this.service = service;
     }
+    @PostMapping
+    public Student save(@RequestBody Student student){
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(@PathVariable("email") String email) {
+        return service.findByEmail(email);
+    }
 
     @GetMapping
     public List<Student> findAllStudents() {
         return service.findAllStudents();
     }
 
+    @PutMapping
+    public Student updateStudent(@RequestBody Student student) {
+        return service .update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void delete(@PathVariable("email") String email) {
+        service.delete(email);
+    }
 }
--- src/main/java/com/jayvon/springdemo/student/StudentService.java
+++ src/main/java/com/jayvon/springdemo/student/StudentService.java
@@ -1,18 +1,16 @@
 package com.jayvon.springdemo.student;
 
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
 
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student("James", "Bond", LocalDate.now(), "jamesbond@777.com", 36),
-                new Student("spongebob", "squarepants", LocalDate.now(), "spongey@bikinibottom.com", 42)
-        );
-    }
+public interface StudentService {
+
+    Student save(Student s);
+
+    List<Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
+
 
 }
